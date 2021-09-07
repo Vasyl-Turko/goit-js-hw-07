968dbb4fbd77cec31470c001b36fa31a01cf7575
Vasyl-Turko <turko7575@gmail.com>
2021-09-08 01:21:41 +0300

    submit1
    hw1 done

---
+++ js/task-01.js
@@ -0,0 +1,10 @@
+// ===== рахуємо кількість li.item
+const mainElements = document.querySelectorAll("li.item");
+const amountMainElements = mainElements.length;
+console.log(`У списку ${amountMainElements} категорії.`);
+
+// ===== знаходимо текстове значення кожного h2
+document.querySelectorAll("h2").forEach(mainElementTitle => { console.log(mainElementTitle.textContent) });
+
+// ====== рахуємо кількість li в кожному li.item
+mainElements.forEach(mainElement => console.log(mainElement.querySelectorAll("li").length));
